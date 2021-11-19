5b5da3f30c426cfbd80040f780c3c448bd011e07
chenall <chenall@users.noreply.github.com>
2021-11-19 12:47:38 +0800

    Update start-grubdev.sh

--- start-grubdev.sh
+++ start-grubdev.sh
@@ -2,9 +2,8 @@
 
 set -e
 sudo apt-get -y update
-
+sudo apt install -y gcc-4.8 gcc-4.8-multilib nasm upx upx-ucl p7zip-full autoconf automake make patch binutils-dev liblzma-dev syslinux isolinux genisoimage
 if [ ! "$INPUT_USEQEMU" = "1" ]; then
-    sudo apt install -y gcc-4.8 gcc-4.8-multilib nasm upx upx-ucl p7zip-full autoconf automake make patch binutils-dev liblzma-dev syslinux isolinux genisoimage
     exit
 fi
 for test in $grub4dos_src
